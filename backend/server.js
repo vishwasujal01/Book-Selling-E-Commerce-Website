461313e90a368583b02b01bdc8b6d5ac7ce857a4
vishwasujal01 <vishwasujal01@gmail.com>
2025-03-31 11:45:26 +0530

    cors correct

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ connectCloudinary();
 app.use(express.json())
 app.use(cors({
     origin: '*',
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true,
 }))
 app.use(express.urlencoded({ extended: true }));
 

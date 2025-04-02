b0d7e1b7deb4cb066eb3355778b1d62f072e0397
vishwasujal01 <vishwasujal01@gmail.com>
2025-04-02 17:22:18 +0530

    solve cors error

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,13 +1,28 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect("mongodb://localhost:27017/Book_Store");
+
+//         console.log("Connected to MongoDB Compass");
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//     }
+// }
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/Book_Store");
 
-        console.log("Connected to MongoDB Compass");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+    mongoose.connection.on('connected', () => {
+        console.log('Connected to MongoDB');
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/Book_Store`)
+
 }
 
 export default connectDB;
